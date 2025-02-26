8322f553d7ece74b34db1fadd7c51146f9618338
Sibo Wang-Chen <sibo.wang@epfl.ch>
2025-02-26 11:33:11 +0100

    Change triggering-related functions into a consolidated class (close #19)

--- arduino/trigger_controller/trigger_controller.ino
+++ arduino/trigger_controller/trigger_controller.ino
@@ -4,7 +4,7 @@
 #include <vector>
 #include <string>
 
-#include "arduinoMessageInterface.hpp"
+#include "arduinoMessageProtocol.hpp"
 #include "constants.hpp"
 
 const int behaviorCameraTriggerPin = 2;
