d7a30eb8f54ba91a82fe2a1c913fb84ed52ab855
Sibo Wang-Chen <sibo.wang@epfl.ch>
2025-04-14 16:03:07 +0200

    revert - adding PCO sources broke Euresys setup

--- recorder/src/muscleRecording.cpp
+++ recorder/src/muscleRecording.cpp
@@ -1,167 +1,167 @@
-#include "muscleRecording.hpp"
+// #include "muscleRecording.hpp"
 
-void muscleImageAcquierer(
-    unsigned int imageSizeXpx,
-    unsigned int imageSizeYpx,
-    unsigned int xOffset,
-    unsigned int yOffset,
-    const RecorderConfig &recorderConfig,
-    std::shared_ptr<MuscleRecordingState> muscleRecordingState,
-    std::shared_ptr<ProgramState> programState,
-    std::shared_ptr<ProgrammedStop> programmedRecordingStop)
-{
-    spdlog::info("Muscle image acquirer thread started");
+// void muscleImageAcquierer(
+//     unsigned int imageSizeXpx,
+//     unsigned int imageSizeYpx,
+//     unsigned int xOffset,
+//     unsigned int yOffset,
+//     const RecorderConfig &recorderConfig,
+//     std::shared_ptr<MuscleRecordingState> muscleRecordingState,
+//     std::shared_ptr<ProgramState> programState,
+//     std::shared_ptr<ProgrammedStop> programmedRecordingStop)
+// {
+//     spdlog::info("Muscle image acquirer thread started");
 
-    unsigned int defaultExposureTimeMicrosecs =
-        recorderConfig.getParameter<int>("muscle_camera",
-                                         "default_exposure_time_us");
+//     unsigned int defaultExposureTimeMicrosecs =
+//         recorderConfig.getParameter<int>("muscle_camera",
+//                                          "default_exposure_time_us");
 
-    // Create muscle camera
-    muscleRecordingState->muscleCamera =
-        std::make_shared<MuscleCamera>(defaultExposureTimeMicrosecs,
-                                       imageSizeXpx,
-                                       imageSizeYpx,
-                                       xOffset,
-                                       yOffset);
-    muscleRecordingState->muscleCamera->start();
+//     // Create muscle camera
+//     muscleRecordingState->muscleCamera =
+//         std::make_shared<MuscleCamera>(defaultExposureTimeMicrosecs,
+//                                        imageSizeXpx,
+//                                        imageSizeYpx,
+//                                        xOffset,
+//                                        yOffset);
+//     muscleRecordingState->muscleCamera->start();
 
-    spdlog::info("Muscle camera configured. Entering frame grabbing loop...");
-    bool isFristFrameRecorded = true;
-    long int currentFrameId = 0;
+//     spdlog::info("Muscle camera configured. Entering frame grabbing loop...");
+//     bool isFristFrameRecorded = true;
+//     long int currentFrameId = 0;
 
-    while (!programState->toQuit.load())
-    {
-        FrameData frameData =
-            muscleRecordingState->muscleCamera->waitForOneFrame();
-        muscleRecordingState
-            ->latestBehaviorFrameHolder
-            ->setLatestFrameData(frameData);
+//     while (!programState->toQuit.load())
+//     {
+//         FrameData frameData =
+//             muscleRecordingState->muscleCamera->waitForOneFrame();
+//         muscleRecordingState
+//             ->latestBehaviorFrameHolder
+//             ->setLatestFrameData(frameData);
 
-        if (programState->isRecording.load())
-        {
-            if (isFristFrameRecorded)
-            {
-                // Reset these to 0 in preparation for the upcoming recording
-                // session
-                currentFrameId = 0;
-                isFristFrameRecorded = false; // toggle off
-            }
-            frameData.frameId = currentFrameId;
-            {
-                std::lock_guard<std::mutex> lock(
-                    muscleRecordingState->muscleImageQueueMutex);
-                muscleRecordingState->muscleImageQueue.push(frameData);
-            }
-            muscleRecordingState->muscleImageQueueCondVar.notify_one();
-        }
+//         if (programState->isRecording.load())
+//         {
+//             if (isFristFrameRecorded)
+//             {
+//                 // Reset these to 0 in preparation for the upcoming recording
+//                 // session
+//                 currentFrameId = 0;
+//                 isFristFrameRecorded = false; // toggle off
+//             }
+//             frameData.frameId = currentFrameId;
+//             {
+//                 std::lock_guard<std::mutex> lock(
+//                     muscleRecordingState->muscleImageQueueMutex);
+//                 muscleRecordingState->muscleImageQueue.push(frameData);
+//             }
+//             muscleRecordingState->muscleImageQueueCondVar.notify_one();
+//         }
 
-        // Whether we've reached a programmed stop
-        // int numFramesExpected =
-        //     programmedRecordingStop->numMuscleFramesExpected;
-        // if (currentFrameId == numFramesExpected - 1 &&
-        //     numFramesExpected >= 0)
-        // {
-        //     // Placeholder - nothing to do here actually because the Arduino
-        //     // will stop triggering the muscle camera by itself
-        //     // Don't toggle programmedRecordingStop->hasEndedFlagForGUI (the
-        //     // behavior acquirer thread will do it)
-        // }
+//         // Whether we've reached a programmed stop
+//         // int numFramesExpected =
+//         //     programmedRecordingStop->numMuscleFramesExpected;
+//         // if (currentFrameId == numFramesExpected - 1 &&
+//         //     numFramesExpected >= 0)
+//         // {
+//         //     // Placeholder - nothing to do here actually because the Arduino
+//         //     // will stop triggering the muscle camera by itself
+//         //     // Don't toggle programmedRecordingStop->hasEndedFlagForGUI (the
+//         //     // behavior acquirer thread will do it)
+//         // }
 
-        currentFrameId++;
-    }
-}
+//         currentFrameId++;
+//     }
+// }
 
-void muscleImageSaver(
-    const RecorderConfig &recorderConfig,
-    std::shared_ptr<MuscleRecordingState> muscleRecordingState,
-    std::shared_ptr<SaveDirectory> saveDirectory,
-    std::shared_ptr<ProgramState> programState,
-    int tiffCompressionMethod)
-{
-    std::thread::id myThreadId = std::this_thread::get_id();
-    std::stringstream ss;
-    ss << myThreadId;
-    std::string threadIdString = ss.str();
-    spdlog::info("Muscle image saver thread started (thread ID {})",
-                 threadIdString);
+// void muscleImageSaver(
+//     const RecorderConfig &recorderConfig,
+//     std::shared_ptr<MuscleRecordingState> muscleRecordingState,
+//     std::shared_ptr<SaveDirectory> saveDirectory,
+//     std::shared_ptr<ProgramState> programState,
+//     int tiffCompressionMethod)
+// {
+//     std::thread::id myThreadId = std::this_thread::get_id();
+//     std::stringstream ss;
+//     ss << myThreadId;
+//     std::string threadIdString = ss.str();
+//     spdlog::info("Muscle image saver thread started (thread ID {})",
+//                  threadIdString);
 
-    std::vector<int> compressionParams;
-    compressionParams.push_back(cv::IMWRITE_TIFF_COMPRESSION);
-    compressionParams.push_back(tiffCompressionMethod);
+//     std::vector<int> compressionParams;
+//     compressionParams.push_back(cv::IMWRITE_TIFF_COMPRESSION);
+//     compressionParams.push_back(tiffCompressionMethod);
 
-    int frameCount = 0;
-    int queueLength = -1;
-    uint64_t startTime = 0;
-    uint64_t walltime = 0;
-    FrameData frameData;
+//     int frameCount = 0;
+//     int queueLength = -1;
+//     uint64_t startTime = 0;
+//     uint64_t walltime = 0;
+//     FrameData frameData;
 
-    int performanceLoggingInterval = recorderConfig.getParameter<int>(
-        "muscle_camera", "saving_performance_logging_interval");
+//     int performanceLoggingInterval = recorderConfig.getParameter<int>(
+//         "muscle_camera", "saving_performance_logging_interval");
 
-    while (!programState->toQuit.load())
-    {
-        {
-            std::unique_lock<std::mutex> lock(
-                muscleRecordingState->muscleImageQueueMutex);
-                muscleRecordingState->muscleImageQueueCondVar.wait(
-                lock, [&muscleRecordingState, programState]
-                { return !muscleRecordingState->muscleImageQueue.empty() ||
-                         programState->toQuit.load(); });
+//     while (!programState->toQuit.load())
+//     {
+//         {
+//             std::unique_lock<std::mutex> lock(
+//                 muscleRecordingState->muscleImageQueueMutex);
+//                 muscleRecordingState->muscleImageQueueCondVar.wait(
+//                 lock, [&muscleRecordingState, programState]
+//                 { return !muscleRecordingState->muscleImageQueue.empty() ||
+//                          programState->toQuit.load(); });
 
-            if (programState->toQuit.load())
-            {
-                spdlog::info(
-                    "Behavior image saver thread is breaking out of loop.");
-                break;
-            }
+//             if (programState->toQuit.load())
+//             {
+//                 spdlog::info(
+//                     "Behavior image saver thread is breaking out of loop.");
+//                 break;
+//             }
 
-            queueLength = muscleRecordingState->muscleImageQueue.size();
-            frameData = muscleRecordingState->muscleImageQueue.front();
-            muscleRecordingState->muscleImageQueue.pop();
-        }
+//             queueLength = muscleRecordingState->muscleImageQueue.size();
+//             frameData = muscleRecordingState->muscleImageQueue.front();
+//             muscleRecordingState->muscleImageQueue.pop();
+//         }
 
-        startTime = getCurrentTimeMicroseconds();
-        std::string filenameStem =
-            "behavior_frame_" + fmt::format("{:09}", frameData.frameId);
-        std::filesystem::path muscleSaveDir =
-            std::filesystem::path(saveDirectory->getDirectory()) /
-            "muscle_images";
+//         startTime = getCurrentTimeMicroseconds();
+//         std::string filenameStem =
+//             "behavior_frame_" + fmt::format("{:09}", frameData.frameId);
+//         std::filesystem::path muscleSaveDir =
+//             std::filesystem::path(saveDirectory->getDirectory()) /
+//             "muscle_images";
 
-        std::string filename = muscleSaveDir / (filenameStem + ".tif");
+//         std::string filename = muscleSaveDir / (filenameStem + ".tif");
 
-        try {
-            cv::imwrite(filename, frameData.image, compressionParams);
-        } catch (const cv::Exception& ex) {
-            spdlog::error("Exception saving image: {}", ex.what());
-        }
+//         try {
+//             cv::imwrite(filename, frameData.image, compressionParams);
+//         } catch (const cv::Exception& ex) {
+//             spdlog::error("Exception saving image: {}", ex.what());
+//         }
         
-        walltime = getCurrentTimeMicroseconds() - startTime;
-        if (frameCount % performanceLoggingInterval)
-        {
-            spdlog::info(
-                "Muscle image saver thread (thread ID {}) reporting: "
-                "{} frames in queue; "
-                "it took {} us to save a single frame",
-                threadIdString, queueLength, walltime);
-        }
-    }
-}
+//         walltime = getCurrentTimeMicroseconds() - startTime;
+//         if (frameCount % performanceLoggingInterval)
+//         {
+//             spdlog::info(
+//                 "Muscle image saver thread (thread ID {}) reporting: "
+//                 "{} frames in queue; "
+//                 "it took {} us to save a single frame",
+//                 threadIdString, queueLength, walltime);
+//         }
+//     }
+// }
 
-void stopMuscleImageSaver(
-    std::shared_ptr<MuscleRecordingState> muscleRecordingState,
-    std::shared_ptr<ProgramState> programState)
-{
-    if (!programState->toQuit.load())
-    {
-        spdlog::critical(
-            "stopMuscleImageSaver() called but toQuit is "
-            "not set to true. This shouldn't happen.");
-        throw std::runtime_error(
-            "stopMuscleImageSaver() called but toQuit is "
-            "not set to true. This shouldn't happen.");
-    }
-    else
-    {
-        muscleRecordingState->muscleImageQueueCondVar.notify_all();
-    }
-}
+// void stopMuscleImageSaver(
+//     std::shared_ptr<MuscleRecordingState> muscleRecordingState,
+//     std::shared_ptr<ProgramState> programState)
+// {
+//     if (!programState->toQuit.load())
+//     {
+//         spdlog::critical(
+//             "stopMuscleImageSaver() called but toQuit is "
+//             "not set to true. This shouldn't happen.");
+//         throw std::runtime_error(
+//             "stopMuscleImageSaver() called but toQuit is "
+//             "not set to true. This shouldn't happen.");
+//     }
+//     else
+//     {
+//         muscleRecordingState->muscleImageQueueCondVar.notify_all();
+//     }
+// }
---
+++ recorder/src/peripherals/_muscleCamera.cpp
@@ -0,0 +1,110 @@
+// #include "muscleCamera.hpp"
+
+// MuscleCamera::MuscleCamera(unsigned int exposureTimeMicrosecs,
+//                            int imageWidth,
+//                            int imageHeight,
+//                            int xOffset,
+//                            int yOffset)
+// {
+//     spdlog::info("Setting up PCO (muscle) camera");
+//     camera_.defaultConfiguration();
+//     pco::Configuration config = camera_.getConfiguration();
+//     spdlog::info("PCO camera default configuration retrieved");
+//     config.roi.x0 = xOffset + 1;
+//     config.roi.y0 = yOffset + 1;
+//     config.roi.x1 = xOffset + imageWidth;
+//     config.roi.y1 = yOffset + imageHeight;
+//     config.trigger_mode = TRIGGER_MODE_EXTERNALTRIGGER;
+//     config.acquire_mode = ACQUIRE_MODE_AUTO;
+//     config.delay_time_s = 0;
+//     config.noise_filter_mode = NOISE_FILTER_MODE_ON;
+//     spdlog::info("Attempting to set PCO camera configuration");
+//     camera_.setConfiguration(config);
+//     spdlog::info("PCO camera configuration set");
+
+//     camera_.setExposureTime(exposureTimeMicrosecs / 1000000.0);
+//     camera_.autoExposureOff();
+//     spdlog::info("PCO camera exposure time set to {} microseconds",
+//                  exposureTimeMicrosecs);
+// }
+
+// MuscleCamera::~MuscleCamera()
+// {
+//     stop();
+// }
+
+// void MuscleCamera::start(unsigned int bufferSize)
+// {
+//     spdlog::info("Starting PCO camera");
+//     camera_.record(bufferSize, pco::RecordMode::ring_buffer);
+//     cameraReadyFlag_.store(true);
+//     spdlog::info("PCO camera started");
+// }
+
+// void MuscleCamera::stop()
+// {
+//     camera_.stop();
+// }
+
+// FrameData MuscleCamera::waitForOneFrame()
+// {
+//     if (!firstFrameHasArrived_)
+//     {
+//         spdlog::debug("Waiting for first frame from PCO camera");
+//         camera_.waitForFirstImage();
+//         firstFrameHasArrived_ = true;
+//     }
+//     else
+//     {
+//         spdlog::debug("Waiting for new image from PCO camera");
+//         camera_.waitForNewImage();
+//     }
+//     spdlog::debug("Image received from PCO camera");
+
+//     uint64_t receivedTime = getCurrentTimeMicroseconds();
+//     camera_.image(currentPCOImage_,
+//                   PCO_RECORDER_LATEST_IMAGE,
+//                   pco::DataFormat::Mono16);
+
+//     FrameData frameData;
+//     frameData.receivedTime = receivedTime;
+//     frameData.image = cv::Mat(imageHeight_,
+//                               imageWidth_,
+//                               CV_16UC1,
+//                               currentPCOImage_.raw_data().first);
+//     return frameData;
+// }
+
+// bool MuscleCamera::isReady() const
+// {
+//     return cameraReadyFlag_.load();
+// }
+
+// void MuscleCamera::setExposureTime(unsigned int exposureTimeMicrosecs)
+// /**
+//  * @brief Set exposure time in microseconds. Note that this can be called
+//  * even during the recording.
+//  */
+// {
+//     camera_.setExposureTime(exposureTimeMicrosecs / 1000000.0);
+// }
+
+// int roundToNearestValidMuscleCamWidth(int initialValue)
+// /**
+//  * @brief ROI width must be a multiple of 32 (see pco.panda 4.2/
+//  * pco.panda 4.2 bi/pco.panda 4.2 bi UV User Manual, Appendix A1.1.)
+//  */
+// {
+//     int remainder = initialValue % 32;
+//     return initialValue - remainder + (remainder < 16 ? 0 : 32);
+// }
+
+// int roundToNearestValidMuscleCamHeight(int initialValue)
+// /**
+//  * @brief ROI height must be a multiple of 8 (see pco.panda 4.2/
+//  * pco.panda 4.2 bi/pco.panda 4.2 bi UV User Manual, Appendix A1.1.)
+//  */
+// {
+//     int remainder = initialValue % 8;
+//     return initialValue - remainder + (remainder < 4 ? 0 : 8);
+// }
