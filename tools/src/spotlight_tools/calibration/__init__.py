73a07a00b23ff780174f30d6f31bd06e2cf560d9
Sibo Wang <sibo.wang@epfl.ch>
2025-10-28 20:06:44 +0100

    remove spotlight-tools (in the 'tools' folder), update readme

--- tools/src/spotlight_tools/calibration/__init__.py
+++
@@ -1,3 +0,0 @@
-from .aruco import ArUcoBoard, detect_aruco, plot_aruco_detections
-from .model import ransac_filter_outliers, visualize_ransac_results
-from .mapper import SpotlightPositionMapper, BehaviorMuscleCrossMapper
